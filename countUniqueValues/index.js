f5dd63ce55e3087e07c39758a4a4ea43fda72009
zkobrinsky <zachkobrinsky@gmail.com>
2021-02-19 10:23:52 -0500

    non-destructive solution w/o Set

---
+++ countUniqueValues/index.js
@@ -0,0 +1,33 @@
+// input:
+// let array = [1, 1, 1, 1, 1, 1, 2];
+let array = [1, 2, 3, 4, 4, 4, 4, 4, 7, 7, 12, 12, 13];
+// let array = [];
+// let array = [-2,-1,-1,0,1]
+
+// return an integer
+// iterate through array
+// use two counters, ... what to call them? could still be left and right
+
+const countUniqueValues = (array) => {
+  let counter = 1;
+  let left = 0;
+  let right = 1;
+
+  if (array.length === 0) {
+    return 0;
+  }
+
+  while (right < array.length) {
+    if (array[left] === array[right] || left === right) {
+      right++;
+    } else {
+      counter++;
+      right++;
+      left = right - 1;
+    }
+  }
+
+  return counter;
+};
+
+console.log(countUniqueValues(array));
