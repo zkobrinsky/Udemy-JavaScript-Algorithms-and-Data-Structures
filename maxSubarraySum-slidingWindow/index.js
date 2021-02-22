3157d448d9dc7de124777946b851d38f9d18ef13
zkobrinsky <zachkobrinsky@gmail.com>
2021-02-22 10:48:08 -0500

    formatting

--- maxSubarraySum-slidingWindow/index.js
+++ maxSubarraySum-slidingWindow/index.js
@@ -1,5 +1,20 @@
-const arr = [4, 2, 1, 6, 2];
-const n = 4;
+// O(n) solution (sliding window)
+function maxSubarraySum(arr, num) {
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < num) return null;
+
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+}
 
 // naive solution
 // function maxSubarraySum(arr, n) {
@@ -22,23 +37,5 @@ const n = 4;
 //   }
 //   return maxSum;
 // }
-
-// O(n) solution (sliding window)
-function maxSubarraySum(arr, num) {
-  let maxSum = 0;
-  let tempSum = 0;
-  if (arr.length < num) return null;
-
-  for (let i = 0; i < num; i++) {
-    maxSum += arr[i];
-  }
-
-  tempSum = maxSum;
-  for (let i = num; i < arr.length; i++) {
-    tempSum = tempSum - arr[i - num] + arr[i];
-    maxSum = Math.max(maxSum, tempSum);
-  }
-  return maxSum;
-}
 
 console.log(maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
