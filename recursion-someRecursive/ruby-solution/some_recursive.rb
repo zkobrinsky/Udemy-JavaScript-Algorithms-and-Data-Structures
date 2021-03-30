41fa33929b07411fc5f20684583f5af829345786
zkobrinsky <zachkobrinsky@gmail.com>
2021-03-30 16:09:14 -0400

    added new test cases

--- recursion-someRecursive/ruby-solution/some_recursive.rb
+++ recursion-someRecursive/ruby-solution/some_recursive.rb
@@ -1,23 +1,30 @@
 require 'pry'
 
-nums = [2,4,6,8]
+nums = [2,4,1,3,8,23]
 
 def is_odd(num)
     num % 2 != 0 ? true : false
 end
 
+def is_one(num)
+    num == 1 ? true : false
+end
+
+def greater_than_ten(num)
+    num > 10 ? true : false
+end
+
 # takes an array and any callback that returns true or false on an integer
 def some_recursive(arr, cb)
     return false if arr.empty?
-  
-    # stores the method into a variable
+
     some_method = method(cb)  
 
-    if some_method.call(arr[0]) == true
+    if some_method.call(arr[-1]) == true
         return true
     end
 
     some_recursive(arr[0...-1], cb)
 end
 
-p some_recursive(nums, :is_odd)
+p some_recursive(nums, :greater_than_ten)
