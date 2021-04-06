f27857b73f5a3d74cba5c8664379ae5e951cbe2e
zkobrinsky <zachkobrinsky@gmail.com>
2021-04-06 13:47:48 -0400

    both colt suggested and personal solutions

---
+++ search-naiveSearchString/index.js
@@ -0,0 +1,34 @@
+// explicitly built from Colt directions
+// function naiveSearchString(str, target) {
+//     let counter = 0;
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] === target[0]) {
+//             for (let j = 0; j < target.length; j++) {
+//                 if(str[i+j] !== target[j]) {
+//                     continue;
+//                 } else {
+//                     if (j === target.length-1) {
+//                         counter++;
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//     return counter;
+// }
+
+// zach solution
+function naiveSearchString(str, target) {
+    let counter = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        let substr = str.slice(i, i+target.length);
+        if (substr === target) {
+            counter++
+        }
+    }
+    return counter;
+}
+
+
+naiveSearchString("ziouefomgwoger", "omg")
