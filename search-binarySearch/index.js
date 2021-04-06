d67d016e1c08a05d9b7282069a0d03b5576af428
zkobrinsky <zachkobrinsky@gmail.com>
2021-04-06 12:32:53 -0400

    js solution

---
+++ search-binarySearch/index.js
@@ -0,0 +1,47 @@
+// colt solution
+// function binarySearch(arr, value) {
+//     let left = 0;
+//     let right = arr.length-1
+//     let middle = Math.floor((left+right)/2)
+
+    
+//     while (arr[middle] !== value) {
+        
+//         if (value < arr[middle]) {
+//             right = middle-1;
+//         } else {
+//             left = middle+1;
+//         }
+//         middle = Math.floor((left+right)/2)
+        
+//         if (arr[middle] === value) {
+//             return middle;
+//         }
+//     }
+//     return -1;
+// }
+
+// zach solution
+function binarySearch(arr, value) {
+    let left = 0;
+    let right = arr.length-1
+    let middle = Math.floor((left+right)/2)
+
+    
+    while (left < right) {
+        
+        if (value < arr[middle]) {
+            right = middle-1;
+        } else {
+            left = middle+1;
+        }
+        middle = Math.floor((left+right)/2)
+        
+        if (arr[middle] === value) {
+            return middle;
+        }
+    }
+    return -1;
+}
+
+console.log(binarySearch([1,2,3,4,5,6,7,8,20],19))
