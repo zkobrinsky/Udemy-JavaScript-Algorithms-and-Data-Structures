0062cfec2905e17577cb404c34a8e5de2c1bebb1
zkobrinsky <zachkobrinsky@gmail.com>
2021-04-06 17:00:14 -0400

    refactored swap

--- search-bubbleSort-recurse-solution/index.js
+++ search-bubbleSort-recurse-solution/index.js
@@ -3,10 +3,9 @@ function bubbleSort(arr) {
         for (let i = 0; i < arr.length-1; i++) {
             if (arr[i] > arr[i+1]) {
                 finished = false;
-                let a = arr[i]
-                let b = arr[i+1]
-                arr[i] = b;
-                arr[i+1] = a;
+                let temp = arr[i]
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
             } else if (arr[i] === arr[i+1]) {
                 continue;
             }
