d5891f01dca9ee9d3076731a9f3df6f2671e96c4
zkobrinsky <zachkobrinsky@gmail.com>
2021-04-24 20:23:31 -0400

    refactored to self-contained function

--- sort-radixSort/index.js
+++ sort-radixSort/index.js
@@ -1,44 +1,42 @@
-function getDigit(num, place) {
-  // zach solution:
-  string = (num / 10 ** place).toString().split(".")[0];
-  return +string[string.length - 1];
-
-  // colt solution:
-  //   return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
-}
-
-function digitCount(num) {
-  // zach solution
-  return Math.floor(num).toString().replace(/-/, "").length;
-
-  // colt solution
-  // return Math.floor(Math.log10(Math.abs(num))) + 1;
-}
-
-function mostDigits(arr) {
-  let digits = -Infinity;
-  for (let i = 0; i < arr.length; i++) {
-    digits = Math.max(digits, digitCount(arr[i]));
-  }
-  return digits;
-}
-
 function radixSort(arr) {
-  let numDigits = mostDigits(arr);
-  for (let k = 0; k < numDigits; k++) {
-    let digitBuckets = Array.from({ length: 10 }, () => []);
-    let negDigitBuckets = Array.from({ length: 10 }, () => []);
-    for (let i = 0; i < arr.length; i++) {
-      let digit = getDigit(arr[i], k);
-      if (Math.sign(arr[i]) === -1) {
-        negDigitBuckets[digit * -1 + 9].push(arr[i]);
-      } else {
-        digitBuckets[digit].push(arr[i]);
-      }
-    }
-    arr = [].concat(...negDigitBuckets, ...digitBuckets);
+  return sort(arr);
+
+  // helpers:
+  function getDigit(num, place) {
+    string = (num / 10 ** place).toString().split(".")[0];
+    return +string[string.length - 1];
   }
-  return arr;
+
+  function digitCount(num) {
+    return Math.floor(num).toString().replace(/-/, "").length;
+  }
+
+  function mostDigits(arr) {
+    let digits = -Infinity;
+    for (let i = 0; i < arr.length; i++) {
+      digits = Math.max(digits, digitCount(arr[i]));
+    }
+    return digits;
+  }
+
+  // the business-end of the function
+  function sort(arr) {
+    let numDigits = mostDigits(arr);
+    for (let k = 0; k < numDigits; k++) {
+      let digitBuckets = Array.from({ length: 10 }, () => []);
+      let negDigitBuckets = Array.from({ length: 10 }, () => []);
+      for (let i = 0; i < arr.length; i++) {
+        let digit = getDigit(arr[i], k);
+        if (Math.sign(arr[i]) === -1) {
+          negDigitBuckets[digit * -1 + 9].push(arr[i]);
+        } else {
+          digitBuckets[digit].push(arr[i]);
+        }
+      }
+      arr = [].concat(...negDigitBuckets, ...digitBuckets);
+    }
+    return arr;
+  }
 }
 
-console.log(radixSort([1234, -56, 7, 72, 98884, -83590, 12, 3, 84]));
+// console.log(radixSort([1234, -56, 7, 72, 98884, -83590, 12, 3, 84]));
