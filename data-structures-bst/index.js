c108a23c5b5ba12a7fd5bfd8fe800f59e3cde972
zkobrinsky <zachkobrinsky@gmail.com>
2021-05-12 16:33:19 -0400

    js insert & find recursively

---
+++ data-structures-bst/index.js
@@ -0,0 +1,58 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val, parent = this.root) {
+    let node = new Node(val);
+    if (!this.root) {
+      this.root = node;
+      return this;
+    }
+
+    if (node.val > parent.val) {
+      if (parent.right) {
+        return this.insert(val, parent.right);
+      } else {
+        parent.right = node;
+        return this;
+      }
+    } else if (node.val < parent.val) {
+      if (parent.left) {
+        return this.insert(val, parent.left);
+      } else {
+        parent.left = node;
+        return this;
+      }
+    }
+    return this;
+  }
+
+  find(val, parent = this.root) {
+    if (!parent) return null;
+    if (val === parent.val) return parent;
+    if (val > parent.val) {
+      if (parent.right) {
+        return this.find(val, parent.right);
+      }
+    } else if (val < parent.val) {
+      if (parent.left) {
+        return this.find(val, parent.left);
+      }
+    }
+    return null;
+  }
+}
+
+let tree = new BST();
+for (let i = 0; i < 10; i++) {
+  tree.insert(Math.floor(Math.random() * 100));
+}
